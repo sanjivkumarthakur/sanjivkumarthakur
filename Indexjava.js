200c7e81bcbf4b7a24409fb4b6554ee933367fd0
sanjivkumarthakur <160305916+sanjivkumarthakur@users.noreply.github.com>
2024-03-11 17:43:00 +0545

    Add files via upload

---
+++ Indexjava.js
@@ -0,0 +1,37 @@
+        // Your existing scripts
+
+        // Function to show the login modal
+        function showLoginModal() {
+            document.getElementById("loginModal").style.display = "block";
+        }
+
+        // Function to check login credentials
+        function checkLogin() {
+            var enteredUsernameOrEmail = document.getElementById("loginUsernameOrEmail").value;
+            var enteredPassword = document.getElementById("loginPassword").value;
+
+            // Check credentials (replace with your authentication logic)
+            if (enteredUsernameOrEmail === "Sanjiv" && enteredPassword === "Jaimatadi1@#") {
+                alert("Login successful! Welcome, Sanjiv Kumar Thakur!");
+                document.getElementById("loginModal").style.display = "none";
+            } else {
+                alert("Invalid credentials. Please try again.");
+            }
+        }
+
+        function changeBackgroundColor() {
+            const colors = ['#ffcc00', '#66ccff', '#ff99cc', '#99ff99', '#ccccff'];
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            document.body.style.backgroundColor = randomColor;
+        }
+		function Facebook()
+		{
+		  window.open("https://www.facebook.com/profile.php?id=100055642376948&mibextid=LQQJ4d");
+		}
+		function whatsapp()
+		{
+		 alert("Whatsapp Number: 9746297764");
+		}
+        function game(){
+            window.open("file:///C:/Users/sharm/OneDrive/Desktop/game.html");
+        }
